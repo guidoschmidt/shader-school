d7cb9c7cf2058f58283702817c57436df1cb79fa
Guido Schmidt <git@guidoschmidt.cc>
2020-08-29 21:42:04 +0200

    EDIT/16-geom-5/rotate.glsl: implement rotation matrix from rodrigues' formula

---
+++ 16-geom-5/rotate.glsl
@@ -0,0 +1,24 @@
+// see: https://semath.info/src/rodrigues-rotation.html
+highp mat4 rotation(highp vec3 n, highp float theta) {
+  // Using Rodrigues' formula, find a matrix which performs a rotation
+  // about the axis n by theta radians
+  float ct = cos(-theta);
+  float st = sin(-theta);
+
+  mat3 rod;
+  rod[0][0] = (1.0 - ct) * n.x * n.x + ct;
+  rod[1][0] = (1.0 - ct) * n.x * n.y + n.z * st;
+  rod[2][0] = (1.0 - ct) * n.x * n.z - n.y * st;
+
+  rod[0][1] = (1.0 - ct) * n.x * n.y - n.z * st;
+  rod[1][1] = (1.0 - ct) * n.y * n.y + ct;
+  rod[2][1] = (1.0 - ct) * n.y * n.z + n.x * st;
+
+  rod[0][2] = (1.0 - ct) * n.x * n.z + n.y * st;
+  rod[1][2] = (1.0 - ct) * n.y * n.z - n.x * st;
+  rod[2][2] = (1.0 - ct) * n.z * n.z + ct;
+  mat4 rotmatrix = mat4(rod);
+  return rotmatrix;
+}
+
+#pragma glslify: export(rotation)
